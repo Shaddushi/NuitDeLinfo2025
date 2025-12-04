5dfc3fbf3b39c7de99ccb25f10403d75866038a2
td1711 <darquestom@gmail.com>
2025-12-04 20:55:46 +0100

    update

--- Frontend/src/components/Game.vue
+++ Frontend/src/components/Game.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="h-100 w-100 bg-light custom-background">
-        <div class="d-flex flex-column h-100">
+        <div class="d-flex flex-column h-100 p-4">
             <div class="flex-grow-1">
                 ici
             </div>
 
-            <div class="p-4">
-                <div class="p-4 bg-white border border-3 border-dark custom-border-2 shadow conversation fs-5">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dolorem ducimus error et eveniet facilis ipsa iste laborum non officia possimus rerum, suscipit voluptatum? Earum fugit necessitatibus temporibus velit voluptatum.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda blanditiis cum dignissimos dolorem facere molestias quasi repudiandae. Aspernatur at dolores eos eum impedit maxime officiis perferendis, quidem quis tempora?
+            <div class="">
+                <div class="bg-white border border-3 border-dark custom-border-2 shadow conversation fs-5 position-relative">
+                    <div class="position-absolute px-4">
+                        <div class="bg-primary rounded-3 px-3 text-white translateY">
+                            Eric Ramat
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda blanditiis cum dignissimos dolorem facere molestias quasi repudiandae. Aspernatur at dolores eos eum impedit maxime officiis perferendis, quidem quis tempora?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dolorem ducimus error et eveniet facilis ipsa iste laborum non officia possimus rerum, suscipit voluptatum? Earum fugit necessitatibus temporibus velit voluptatum.
+                    </div>
+
                 </div>
             </div>
 
@@ -43,5 +51,9 @@ export default {
 
 .conversation{
     min-height: 200px;
+}
+
+.translateY{
+    transform: translateY(-50%);
 }
 </style>
