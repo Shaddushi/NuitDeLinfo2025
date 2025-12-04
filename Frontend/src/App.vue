9f1f75f0a51607a6f9f9f572b6844d2970366c4f
Shaddushi <duportail.honore@gmail.com>
2025-12-04 19:07:41 +0100

    css added

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+  import SnakeGame from './components/snake.vue';
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <SnakeGame />
 </template>
 
 <style scoped></style>
---
+++ Frontend/src/components/snake.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const canvasRef = ref(null);
+let context = null;
+
+onMounted(() => {
+  const canvas = canvasRef.value;
+  context = canvas.getContext("2d");
+
+  context.fillStyle = "red";
+  context.fillRect(10, 10, 30, 30);
+});
+</script>
+
+<template>
+  <canvas ref="canvasRef" class="snake-game"></canvas>
+</template>
+
+<style scoped>
+.snake-game {
+  width: 10vw;
+  height: 100vh;
+  background: #eee;
+}
+</style>
